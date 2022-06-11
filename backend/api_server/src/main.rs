ee90390e7e5d0dbd95c8fca633aa195a96190a07
GautierED <gautierescallierduront@gmail.com>
2022-06-11 20:28:42 +0200

    minor changes

--- backend/api_server/src/main.rs
+++ backend/api_server/src/main.rs
@@ -1,27 +1,28 @@
 use actix_web::{web, App, HttpServer, Result};
 use sqlx::PgPool;
 
-
 mod handlers;
-
 
 #[actix_web::main]
 async fn main() -> Result<(), sqlx::Error> {
-
     dotenv::dotenv().ok();
+    println!("Starting server...");
+
+    let port = std::env::var("port").expect("PORT must be set");
 
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let pool = PgPool::connect(&database_url).await?;
+    println!("Connected to database");
 
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(pool.clone()))
             .route("/user/{id}", web::get().to(handlers::get_user))
             .route("/users", web::get().to(handlers::get_users))
-            .route("/add_user", web::post().to(handlers::add_user))
-            .route("/delete/{id}", web::delete().to(handlers::delete_user))
+            .route("/user", web::post().to(handlers::add_user))
+            .route("/user/{id}", web::delete().to(handlers::delete_user))
         })
-        .bind("127.0.0.1:8080")?
+        .bind(&port)?
         .run()
         .await?;
 
